d65f2b7b46abf98e9904bd60a89f41cb5d44cf89
Sarmad Ashkuri <sarmadashkuri@gmail.com>
2023-08-14 14:57:54 -0400

    set up tables and columns

---
+++ data/migrations/20230814170617_initial-migration.js
@@ -0,0 +1,60 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+  await knex.schema
+    .createTable('projects', table => {
+        table.increments('project_id')
+        table.string('project_name', 128).notNullable()
+        table.string('project_description', 256)
+        table.boolean('project_completed').defaultTo(false).notNullable()
+    })
+    .createTable('resources', table => {
+        table.increments('resource_id')
+        table.string('resource_name', 128).notNullable()
+        table.string('resource_description', 256)
+    })
+    .createTable('tasks', table => {
+        table.increments('task_id')
+        table.string('task_description', 256).notNullable()
+        table.string('task_notes', 256)
+        table.boolean('task_completed').defaultTo(false).notNullable()
+        table.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('project_id')
+            .inTable('projects')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE')
+    })
+    .createTable('project_resources', table => {
+        table.increments('id').primary();
+        table.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('project_id')
+            .inTable('projects')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE')
+        table.integer('resource_id')
+            .unsigned()
+            .notNullable()
+            .references('resource_id')
+            .inTable('resources')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE')
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+  await knex.schema
+    .dropTable('project_resources')
+    .dropTable('tasks')
+    .dropTable('resources')
+    .dropTable('projects')
+};
